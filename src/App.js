bf535b005c98e9d3f18859d0f7ea4848aed37267
Yvann Noto <yvann.noto@mendix.com>
2022-08-15 14:32:54 +0200

    Create a List component that maps the provided list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+const welcome = {
+  title: 'React',
+  greeting: 'Hey'
+};
+
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov and Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+];
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <a href={item.url} target="_blank">{getTitle(item.title)}</a>
+            <span>&nbsp;by {item.author}</span>
+            <div>
+              <span>{item.num_comments} comments</span>
+              <span> | </span>
+              <span>{item.points} points</span>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{welcome.greeting} {getTitle(welcome.title)}!</h1>
+      <List />
+      <div>
+        <label htmlFor="search">Search : </label>
+        <input type="text" id="search" />
+      </div>
     </div>
   );
 }
